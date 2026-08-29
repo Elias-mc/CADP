c4a0cc9ea7abf444a8ecbe897d379fa0dc1dd2ea
Elias <macayzamora1234@gmail.com>
2026-08-28 22:53:47 -0300

    Pactica 2

---
+++ Práctica/Práctica 2/Práctica 2 (parte 1) – Funciones y procedimientos/1.pas
@@ -0,0 +1,34 @@
+// Dado el siguiente programa, indicar qué imprime.
+
+program alcance1;
+var a,b: integer;
+
+procedure uno;
+    var b: integer;
+    begin
+        b := 3;
+        writeln(b);
+    end;
+
+begin
+    a:= 1;
+    b:= 2;
+    uno;
+    writeln(b, a);
+end.
+
+// El programa imprime:
+//     3
+//     21
+// Tenemos dos variables llamadas b:
+// b global → pertenece al programa principal.
+// b local → pertenece al procedimiento uno.
+// Cuando se ejecuta:
+//     a := 1;
+//     b := 2;
+//     uno;
+// Dentro de uno:
+//     b := 3;
+//     writeln(b);
+// Ese b es el local, por lo que imprime: 3
+// Al terminar uno, la variable local desaparece y la b global sigue valiendo 2.
